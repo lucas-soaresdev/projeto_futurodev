6a7e66664f64b4275208fdeccf66f827f1bff86c
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-22 23:14:11 -0300

    adição de animações

---
+++ index.js
@@ -0,0 +1,74 @@
+// Função para o menu mobile
+function abrir(){
+    let Main = document.querySelector('main')
+    let Footer = document.querySelector('footer')
+    let tracos = document.getElementById('tr')
+    let menuMobile = document.getElementById('menu-items')
+
+    const target = document.getElementById('animacao1')
+    const animation = 'menu-animacao'
+
+    if(menuMobile.classList.contains('aberto')){
+        menuMobile.classList.remove('aberto')
+        tracos.classList.remove('menu-animacao')
+        Main.classList.remove('esconder')
+        Footer.classList.remove('esconder')
+
+    }
+    else{
+        menuMobile.classList.add('aberto')
+        tracos.classList.add('menu-animacao')
+        Main.classList.add('esconder')
+        Footer.classList.add('esconder')
+    }
+}
+
+
+
+const animationElements = document.querySelectorAll('.animation');
+
+// Função para verificar a posição dos elementos em relação à janela de visualização
+function checkPositions() {
+  for (let i = 0; i < animationElements.length; i++) {
+    const content = animationElements[i];
+    const elementPosition = content.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementPosition < windowHeight) {
+      content.classList.add('fade-in');
+    }
+  }
+}
+
+// Função para verificar se o elemento está visível na janela de visualização
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Função para adicionar a classe "fade-in" ao elemento quando estiver visível
+function fadeInElement() {
+  for (let i = 0; i < animationElements.length; i++) {
+    const content = animationElements[i];
+    if (isElementInViewport(content)) {
+      content.classList.add('fade-in');
+    }
+  }
+}
+
+
+window.addEventListener('scroll', function() {
+  checkPositions();
+  fadeInElement();
+});
+
+
+window.addEventListener('load', function() {
+  checkPositions();
+  fadeInElement();
+});
