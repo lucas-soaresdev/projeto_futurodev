b9585c9973585dffbe7c9fae21d63479f09884a1
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-08-09 13:04:28 -0300

    refatorei a lógica do menu mobile

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ function abrir(){
     let Footer = document.querySelector('footer')
     let menuOculto = document.getElementById('menu-oculto')
     let menuMobile = document.getElementById('menu-items')
-
-    if(menuMobile.classList.contains('aberto')){
-        menuMobile.classList.remove('aberto')
-        menuOculto.classList.remove('menu-animacao')
-        Main.classList.remove('esconder')
-        Footer.classList.remove('esconder')
-
-    }
-    else{
-        menuMobile.classList.add('aberto')
-        menuOculto.classList.add('menu-animacao')
-        Main.classList.add('esconder')
-        Footer.classList.add('esconder')
-    }
+    
+        menuMobile.classList.toggle('aberto')
+        menuOculto.classList.toggle('menu-animacao')
+        Main.classList.toggle('esconder')
+        Footer.classList.toggle('esconder')
 }
 
 
